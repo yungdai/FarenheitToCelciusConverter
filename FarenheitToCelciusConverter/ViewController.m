511b16839cfbddc517dec164159df05d343870fd
Yung Dai <yungchidai@gmail.com>
2015-04-19 12:17:15 -0400

    updated code and positioning

--- FarenheitToCelciusConverter/ViewController.m
+++ FarenheitToCelciusConverter/ViewController.m
@@ -15,21 +15,14 @@
 
 @implementation ViewController
 
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        _temperatureResultLabel = @"";
-    }
-    return self;
-}
+
 
 // here are the two methods to do the take the temperatureInput and convert them to the respective temperature values
 - (void)convertFarenheitToCelsius {
     double farenheit = [_temperatureInput.text doubleValue];
     double celcius = (farenheit - 32) / 1.8;
     
-    NSString *resultLabel = [[NSString alloc]initWithFormat:@"%.0f°F is %.1f in °C", farenheit, celcius];
+    NSString *resultLabel = [[NSString alloc]initWithFormat:@"%.0f°F is %.1f in Celsius", farenheit, celcius];
     _temperatureResultLabel.text = resultLabel;
 }
 
@@ -38,7 +31,7 @@
     double celsius = [_temperatureInput.text doubleValue];
     double farenheit = (celsius * 1.8) +32;
     
-    NSString *resultLabel = [[NSString alloc]initWithFormat:@"%.0f°C is %.1f in °F", celsius, farenheit];
+    NSString *resultLabel = [[NSString alloc]initWithFormat:@"%.0f°C is %.1f in Farenheit", celsius, farenheit];
     
     _temperatureResultLabel.text = resultLabel;
 }
@@ -47,7 +40,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    
+
     // Do any additional setup after loading the view, typically from a nib.
     
 }
@@ -77,7 +70,7 @@
 }
 
 // this code will hide the keyboard when the return key on the keyboard is pressed
-- (IBAction)temperatureResultLableReturn:(id)sender {
+- (IBAction)temperatureInputReturn:(id)sender {
     [sender resignFirstResponder];
 }
 
